8ced6673cbd8841915599e5fc08685f916c89ffa
Ermis Doulos <me@ermisdoulos.com>
2024-04-06 12:44:54 +0300

    added init.el and some basic settings

---
+++ init.el
@@ -0,0 +1,12 @@
+;; General settings
+(setq inhibit-startup-message t)	;don't show the splash screen
+;;(setq visible-bell t)			;Flash when the bell rings
+(tool-bar-mode -1)			;Don't show tool-bar
+(scroll-bar-mode -1)			;Don't show scroll bar
+
+;; Themes
+;(load-theme 'modus-vivendi t)		;Load the modus vivendi theme
+
+;; Programming
+
+;; Language support
