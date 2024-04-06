669db1350ea90f9cdd98ef8620be8d7f5b166dbc
Ermis Doulos <me@ermisdoulos.com>
2024-04-06 18:33:44 +0300

    added early init to maximize the window

--- init.el
+++ init.el
@@ -3,9 +3,12 @@
 ;;(setq visible-bell t)			;Flash when the bell rings
 (tool-bar-mode -1)			;Don't show tool-bar
 (scroll-bar-mode -1)			;Don't show scroll bar
+;; (custom-set-variables
+ ;; '(initial-frame-alist (quote ((fullscreen . maximized)))))
 
 ;; Themes
-;(load-theme 'modus-vivendi t)		;Load the modus vivendi theme
+(load-theme 'modus-vivendi t)		;Load the modus vivendi theme
+;; (load-theme 'ef-theme t)
 
 ;; Programming
 
