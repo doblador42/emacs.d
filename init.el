6c561408f25b9e71044ec33a2d55cbb3e0031894
Ermis Doulos <me@ermisdoulos.com>
2024-04-20 19:33:36 +0300

    added latexmk

--- init.el
+++ init.el
@@ -30,10 +30,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(lsp-mode undo-tree)))
+ '(LaTeX-command "latexmk")
+ '(TeX-engine 'luatex)
+ '(package-selected-packages '(auctex-latexmk auctex company lsp-mode undo-tree)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(require 'auctex-latexmk)
+(auctex-latexmk-setup)
